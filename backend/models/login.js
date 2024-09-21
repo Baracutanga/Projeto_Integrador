479b34381de626e7f326d369c94ff069a7175130
AntonioMontenegro-Glich <antoniolemos2004@gmail.com>
2024-09-21 16:22:11 -0300

    Primeira parte modelos

--- backend/models/login.js
+++ backend/models/login.js
@@ -2,19 +2,9 @@ const mongoose = require('mongoose');
 
 const loginSchema = new mongoose.Schema({
     email: {type:Mixed, required:true},
-    nome: { type: String, required: true, unique: true },
     senha: { type: Mixed, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   });
-  
-  UserSchema.pre('save', async function(next) {
-    if (this.isModified('password')) {
-      this.password = await bcrypt.hash(this.password, 10);
-    }
-    next();
-  });
-  
-  loginSchema.methods.compareSenha = function(senha) {
-    return bcrypt.compare(senha, this.senha);
-  };
-  
+
   module.exports = mongoose.model('login', loginSchema);
+ 
