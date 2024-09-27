30ee9d5df8a273966428d1f418ca7ac12c579480
AntonioMontenegro-Glich <antoniolemos2004@gmail.com>
2024-09-26 23:51:17 -0300

    rotas

---
+++ backend/routes/professorRoutes.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const professor = require('../controllers/professorController');
+
+router.get('/', professor.getAllController);
+router.post('/', professor.postController);
+router.put('/:id', professor.updateController);
+router.delete('/:id', professor.deleteController);
+
+module.exports = router;
