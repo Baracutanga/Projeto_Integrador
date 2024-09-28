77a941660e0c62e8304b9f71ca80f20de1bab1b0
AntonioMontenegro-Glich <antoniolemos2004@gmail.com>
2024-09-28 14:15:29 -0300

    Ajustando rotas

--- backend/routes/alunoRoutes.js
+++ backend/routes/alunoRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const aluno = require('../controllers/alunoController');
 
 router.get('/', aluno.getAllController);
+router.get('/', aluno.getAlunosById);
 router.post('/', aluno.postController);
 router.put('/:id', aluno.updateController);
 router.delete('/:id', aluno.deleteController);
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/userController');
 
 router.get('/', userController.getAllController);
+router.get('/', aluno.getUsersById);
 router.post('/', userController.postController);
 router.put('/:id', userController.updateController);
 router.delete('/:id', userController.deleteController);
