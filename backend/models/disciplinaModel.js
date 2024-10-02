4f640c52b4a73f92bc0eeb938baefe7cd83e1dc8
israelsz7 <israelsqpessoal@gmail.com>
2024-10-02 08:36:49 -0300

    Colocando alterações finais na minha branch

--- backend/models/disciplinaModel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-
-const disciplinaSchema = new mongoose.Schema({
-  nome: { type: String, required: true },
-});
-//Exportando o modelo de disciplina
-module.exports = mongoose.model('Disciplina', disciplinaSchema);
